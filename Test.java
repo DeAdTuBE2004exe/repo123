7c0cea753f1a591449a73cf7adfff17b0a8cb108
DeAdTuBE2004exe <bharatkulkarni.0000@gmail.com>
2025-04-23 08:23:17 +0530

    uploading Test.java

---
+++ Test.java
@@ -0,0 +1,11 @@
+public class Test {
+
+    public void display() {
+        System.out.printf("I am Bharat\n");
+    }
+
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.display();
+    }
+}
